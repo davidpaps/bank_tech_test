2b7a8c98f679fcbca9aa85ca130ea4f7f7476217
David Papamichael <davidpapamichael@gmail.com>
2020-02-24 18:32:18 +0000

    date function added to calculate the date for transactions

--- src/bank.js
+++ src/bank.js
@@ -15,22 +15,32 @@ Bank.prototype.transactionHistory = function(){
 
 Bank.prototype.deposit = function(money){
   this.balance += money;
-  var transactionBalance =  this.currentBalance();
-  if (this.transactions.length < 1) {
-    this.transactions.push(`10/01/2014 || ${money.toFixed(2)} || || ${transactionBalance.toFixed(2)}`);
-  }
-  else {
-    this.transactions.push(`13/01/2014 || ${money.toFixed(2)} || || ${transactionBalance.toFixed(2)}`);
-  }
+  this._updateStatement(money)
+}
+
+Bank.prototype.processingDate = function(){
+  var date = new Date();
+  var dd = date.getDate();
+  var mm = date.getMonth() + 1;
+  var yyyy = date.getFullYear();
+  return date = dd + '/' + mm + '/' + yyyy
 }
 
 Bank.prototype.withdraw = function(money){
   this.balance -= money;
-  var transactionBalance =  this.currentBalance();
-  this.transactions.push(`14/01/2014 || || ${money.toFixed(2)} || ${transactionBalance.toFixed(2)}`);
+  this.transactions.push(`14/01/2014 || || ${money.toFixed(2)} || ${this.currentBalance().toFixed(2)}`);
 }
 
 Bank.prototype.printStatement = function(){
   var header = 'date || credit || debit || balance';
   return (header + '\n' + this.transactions.reverse().join('\n'))
+}
+
+Bank.prototype._updateStatement = function(money){
+  if (this.transactions.length < 1) {
+    this.transactionHistory().push(`10/01/2014 || ${money.toFixed(2)} || || ${this.currentBalance().toFixed(2)}`);
+  }
+  else {
+    this.transactionHistory().push(`13/01/2014 || ${money.toFixed(2)} || || ${this.currentBalance().toFixed(2)}`);
+  }
 }
